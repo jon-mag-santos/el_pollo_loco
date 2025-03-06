25930e6adf905bd5964464d57473ea09468c4487
jon-mag-santos <jon.mag.santos@gmail.com>
2025-03-07 00:21:50 +0100

    status bar endboss setting

--- models/endboss-healthbar.class.js
+++ models/endboss-healthbar.class.js
@@ -7,12 +7,12 @@
 class EndbossHealthbar extends DrawableObject {
     IMAGES_BOSS_HEALTH_FULL = ['img/7_statusbars/2_statusbar_endboss/blue.png'];
     IMAGES_BOSS_HEALTH = [
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/0.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/20.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/40.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/60.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/80.png',
-        'img/7_statusbars/1_statusbar/2_statusbar_health/blue/100.png',
+        'img/7_statusbars/2_statusbar_endboss/blue/0.png',
+        'img/7_statusbars/2_statusbar_endboss/blue/20.png',
+        'iimg/7_statusbars/2_statusbar_endboss/blue/40.png',
+        'iimg/7_statusbars/2_statusbar_endboss/blue/60.png',
+        'iimg/7_statusbars/2_statusbar_endboss/blue/80.png',
+        'iimg/7_statusbars/2_statusbar_endboss/blue/100.png',
     ];
 
     bossEnergy = 100;
