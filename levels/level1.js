389d1896e5d2b03745e5f47957456bc981f2314b
jon-mag-santos <jon.mag.santos@gmail.com>
2025-03-15 21:55:41 +0100

    global variable canvasmaxwidth implemented

--- levels/level1.js
+++ levels/level1.js
@@ -1,46 +1,51 @@
 /**
+ * Globale variable to define canvas maximal width
+ */
+const canvasMaxWidth = 719;
+
+/**
  * Function to initialize the game level with  enemies, clouds, background objects, coins, bottles, and an endboss.
  */
 function initGameLevel() {
     level1 = new Level(
         // Background objects array
         [
-            new BackgroundObject('img/5_background/layers/air.png', -719),
-            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -719),
-            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -719),
-            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -719),
+            new BackgroundObject('img/5_background/layers/air.png', -canvasMaxWidth),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', -canvasMaxWidth),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', -canvasMaxWidth),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', -canvasMaxWidth),
             new BackgroundObject('img/5_background/layers/air.png', 0),
             new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 0),
             new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 0),
             new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 0),
-            new BackgroundObject('img/5_background/layers/air.png', 719),
-            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719),
-            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719),
-            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719),
-            new BackgroundObject('img/5_background/layers/air.png', 719 * 2),
-            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 2),
-            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 2),
-            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 2),
-            new BackgroundObject('img/5_background/layers/air.png', 719 * 3),
-            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 3),
-            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 3),
-            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 3),
-            new BackgroundObject('img/5_background/layers/air.png', 719 * 4),
-            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 4),
-            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 4),
-            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 4),
-            new BackgroundObject('img/5_background/layers/air.png', 719 * 5),
-            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 5),
-            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 5),
-            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 5),
-            new BackgroundObject('img/5_background/layers/air.png', 719 * 6),
-            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', 719 * 6),
-            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', 719 * 6),
-            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', 719 * 6),
-            new BackgroundObject('img/5_background/layers/air.png', 719 * 7),
-            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', 719 * 7),
-            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', 719 * 7),
-            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', 719 * 7)
+            new BackgroundObject('img/5_background/layers/air.png', canvasMaxWidth),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', canvasMaxWidth),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', canvasMaxWidth),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', canvasMaxWidth),
+            new BackgroundObject('img/5_background/layers/air.png', canvasMaxWidth * 2),
+            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', canvasMaxWidth * 2),
+            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', canvasMaxWidth * 2),
+            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', canvasMaxWidth * 2),
+            new BackgroundObject('img/5_background/layers/air.png', canvasMaxWidth * 3),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', canvasMaxWidth * 3),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', canvasMaxWidth * 3),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', canvasMaxWidth * 3),
+            new BackgroundObject('img/5_background/layers/air.png', canvasMaxWidth * 4),
+            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', canvasMaxWidth * 4),
+            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', canvasMaxWidth * 4),
+            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', canvasMaxWidth * 4),
+            new BackgroundObject('img/5_background/layers/air.png', canvasMaxWidth * 5),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', canvasMaxWidth * 5),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', canvasMaxWidth * 5),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', canvasMaxWidth * 5),
+            new BackgroundObject('img/5_background/layers/air.png', canvasMaxWidth * 6),
+            new BackgroundObject('img/5_background/layers/3_third_layer/1.png', canvasMaxWidth * 6),
+            new BackgroundObject('img/5_background/layers/2_second_layer/1.png', canvasMaxWidth * 6),
+            new BackgroundObject('img/5_background/layers/1_first_layer/1.png', canvasMaxWidth * 6),
+            new BackgroundObject('img/5_background/layers/air.png', canvasMaxWidth * 7),
+            new BackgroundObject('img/5_background/layers/3_third_layer/2.png', canvasMaxWidth * 7),
+            new BackgroundObject('img/5_background/layers/2_second_layer/2.png', canvasMaxWidth * 7),
+            new BackgroundObject('img/5_background/layers/1_first_layer/2.png', canvasMaxWidth * 7)
         ],
         // Clouds array
         [
