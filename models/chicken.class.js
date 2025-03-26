8918dd27164604f796c67b7fe02f2ed698cf16f3
jon-mag-santos <jon.mag.santos@gmail.com>
2025-03-26 10:30:13 +0100

    enemies speed upgraded & bottles positions redefined

--- models/chicken.class.js
+++ models/chicken.class.js
@@ -3,7 +3,7 @@ class Chicken extends MoveableObject {
     y = 350;
     width = 100;
     height = 80;
-    speed = 5;
+    speed = 7 + (Math.random() * 3 + 1);
     energy = 1;
     offset = {
         top: 5,
